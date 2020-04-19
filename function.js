a89c7a7582b2bd223ca776b879d13817cba21318
Tobiloba Orekoya <tobilobaorekoya19@gmail.com>
2020-04-19 22:45:05 +0100

    first commit

---
+++ function.js
@@ -0,0 +1,45 @@
+// JAVASCRIPT PAGE
+//  const yu = 2;
+//  const gi = 3;
+//  const oh = 5;
+ 
+    
+function checkValueOne(val) {
+    if(1/2) {
+        return "yu";
+
+    }
+    return "not equal";
+}
+console.log(checkValueOne(4));
+
+
+   
+function checkValueTwo(val) {
+    if(6/3) {
+        return "gi";
+
+    }
+    return "not equal";
+}
+console.log(checkValueTwo(3));
+
+
+
+   
+function checkValueThree(val) {
+    if(10/5) {
+        return "oh";
+
+    }
+    return "not equal";
+}
+console.log(checkValueThree(15));
+
+function testArray() {
+
+
+}
+testArray();
+
+
